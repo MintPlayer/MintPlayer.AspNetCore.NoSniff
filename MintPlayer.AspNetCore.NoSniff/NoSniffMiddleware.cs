800f5010e5699ced8c28d465b879e2bdf016b657
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2021-12-18 15:42:23 +0100

    Add middleware

---
+++ MintPlayer.AspNetCore.NoSniff/NoSniffMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace MintPlayer.AspNetCore.NoSniff
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class NoSniffMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public NoSniffMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await next(httpContext);
+            httpContext.Response.Headers.XContentTypeOptions = "nosniff";
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class NoSniffMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseNoSniffMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<NoSniffMiddleware>();
+        }
+    }
+}
